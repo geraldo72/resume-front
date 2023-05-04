f010a2e041c5f3be87cdd630cf5057d391c5d8d6
Luis Geraldo de Ramos <13860067+geraldo72@users.noreply.github.com>
2023-05-03 22:29:56 -0300

    GH Pages

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,4 +1,6 @@
 /** @type {import('./$types').PageLoad} */
+export const prerender = true;
+
 export async function load({params}) {
     const response = await fetch('https://gist.githubusercontent.com/geraldo72/57ec0c42828c2d3409f9995cad0da13c/raw/');
     return await response.json();
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    export const prerender = true;
 import Profile from '$lib/Profile.svelte'
 import History from '$lib/History.svelte'
 
